610f420242b454b99c1af4ae0f4ef575883f0685
upon-X <valemiche003@gmail.com>
2023-08-19 20:04:25 -0300

    actualized

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,23 @@
 import styles from './Home.module.css'
+import { Link } from 'react-router-dom'
+
 
 export default function Home() {
     return (
         <div className={styles.home}>
-            <p className={styles.name}>
-                Valentino Micheloni
-                <span className={styles.role}>
+            <div className={styles.title}>
+                <p className={styles.name}>
+                    Valentino Micheloni
+                </p>
+                <p className={styles.role}>
                     Full Stack Web Developer
-                </span>
-            </p>
+                </p>
+            </div>
+            <div>
+                <p className={styles.description}>
+                    Welcome to my portfolio. You can see and interact with my <Link to='/projects' className={styles.toprojects}>projects</Link>
+                </p>
+            </div>
         </div>
     )
 }
