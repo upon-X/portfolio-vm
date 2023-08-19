79a52be2e3fc3ee403107014f1afed70b8cfc0bc
upon-X <valemiche003@gmail.com>
2023-08-19 14:26:28 -0300

    intialized portfolio

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import styles from './Home.module.css'
+
+export default function Home() {
+    return (
+        <div className={styles.home}>
+            <div>
+                Hola sofi
+            </div>
+        </div>
+    )
+}
