c797ace359662749b24e094ff8a6cc101b545d45
upon-X <valemiche003@gmail.com>
2023-08-26 16:21:22 -0300

    Falta llenar el Contact

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='*' element={<Home />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
