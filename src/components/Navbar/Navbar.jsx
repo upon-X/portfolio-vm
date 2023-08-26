22a21d3e41e8308a707d20638524265f0af570cb
upon-X <valemiche003@gmail.com>
2023-08-26 12:38:45 -0300

    ultimos toques

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,12 +5,14 @@ import logo from '../../assets/logo.ico'
 export default function Navbar() {
     return (
         <div className={styles.navbar}>
-            <div className={styles.logo}>
-                <Link to='/'><img className={styles.logo_img} src={logo} alt="logo" /></Link>
-            </div>
-            <div className={styles.links}>
-                <Link to='/projects' className={styles.link_projects}>Projects</Link>
-                <Link to='/contact' className={styles.link_contact}>Contact</Link>
+            <div className={styles.limit_navbar}>
+                <div className={styles.logo}>
+                    <Link to='/'><img className={styles.logo_img} src={logo} alt="logo" /></Link>
+                </div>
+                <div className={styles.links}>
+                    <Link to='/projects' className={styles.link_projects}>Projects</Link>
+                    <Link to='/contact' className={styles.link_contact}>Contact</Link>
+                </div>
             </div>
         </div>
     )
